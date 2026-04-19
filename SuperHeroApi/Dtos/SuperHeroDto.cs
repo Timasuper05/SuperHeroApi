135245af083cd37173112540a22b0292a58c59dc
Tima <76902286+Timasuper05@users.noreply.github.com>
2026-04-19 12:01:29 +0000

    as

--- SuperHeroApi/Dtos/SuperHeroDto.cs
+++ SuperHeroApi/Dtos/SuperHeroDto.cs
@@ -6,6 +6,6 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Place { get; set; } = string.Empty; 
-        public string Pla { get; set; } = string.Empty;
+        public string Plaсe1 { get; set; } = string.Empty;
     }
 }
